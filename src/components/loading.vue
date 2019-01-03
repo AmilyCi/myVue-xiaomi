269b2061fabdd83f32fc6596a7b3702d775f1d88
AmilyCi <cixinii@163.com>
2019-01-03 18:41:40 +0800

    add loading

---
+++ src/components/loading.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="base load-once" :class="{remove: !loadState}" v-show="loadStatus"></div>
+    <transition name="fade">
+      <div class="base load" v-if="load"></div>
+    </transition>
+  </div>
+</template>
+<script>
+  export default {
+    props: [load],
+    data (){
+      return {
+        loadState: true,
+        loadStatus: true
+      }
+    },
+    mounted (){
+      let mi = this;
+      setTimeout(function(){
+        mi.loadState = false;
+        setTimeout(function(){
+          mi.loadStatus = false;
+        },500)
+      },1500)
+    }
+  }
+</script>
+<style lang='less'>
+  .base{
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: 200;
+  }
+  .load-once{
+    background: url(../images/mi_load.png) no-repeat center center;
+    background-size: 80px;
+    opacity: 1;
+  }
+</style>
