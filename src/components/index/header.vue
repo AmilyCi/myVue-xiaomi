83e227e35fa8530221e2c8c985f6a92e9ac2cc4c
AmilyCi <cixinii@163.com>
2019-01-08 18:57:38 +0800

    header

---
+++ src/components/index/header.vue
@@ -0,0 +1,162 @@
+<template>
+  <div>
+    <div :class="{screen: focus}">
+      <div class="top">
+        <div class="bg" :style="bgOpacSty" v-show="!focus"></div>
+        <div class="bg1" v-show="focus"></div>
+        <div class="main-head">
+          <div class="left logo" v-show="!focus"></div>
+          <div class="left back" @click="goBackEvent" v-show="focus"></div>
+          <div class="center">
+            <input type='text' :class="{border:focus}" @focus="handleFocus" v-model="inputText">
+          </div>
+          <div class="right" v-show="!focus">登录</div>
+          <div class="right search" v-show="focus"></div>
+        </div>
+      </div>
+      <div class="main" v-show="focus">
+        <div class="title">热门搜索</div>
+        <img :src="searchImg">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import data from '../../../data.json';
+  export default {
+    props: ['opac'],
+    data () {
+      return {
+        focus: false,
+        bgOpacSty: {
+          opacity: 1
+        },
+        inputText: '',
+        searchImg: '',
+      }
+    },
+    created () {
+      this.searchImg = data.searchImg;
+    },
+    watch: {
+      opac () {
+        this.bgOpacSty.opacity = this.opac;
+      }
+    },
+    methods: {
+      goBackEvent () {
+        this.focus = false;
+        this.$emit('searchEvent',false);
+      },
+      handleFocus () {
+        this.focus = true;
+        this.$emit('searchEvent',true);
+      }
+    }
+  } 
+</script>
+
+<style lang='less' scoped>
+  .screen {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: block;
+    background: #fff;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+  }
+  .absolute100 {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  .top {
+    width: 100%;
+    height: 40px;
+    font-family: 'Microsoft YaHei';
+    font-size: 0;
+    position: fixed;
+    color: #fff;
+    .bg {
+      .absolute100;
+      background: #ff6700;
+    }
+    .bg1 {
+      .absolute100;
+      background: #efefef;
+    }
+    .main-head {
+      .absolute100;
+    }
+    .left {
+      width: 15%;
+      line-height: 40px;
+      text-align: center;
+      font-size: 0;
+      display: inline-block;
+      &.logo {
+        width: 15%;
+        height: 40px;
+        background: url('../../images/svg/mi.svg') no-repeat center center;
+        background-size: 30px;
+      }
+      &.back {
+        width: 15%;
+        height: 40px;
+        background: url('../../images/svg/back.svg') no-repeat center center;
+        background-size: 20px;
+      }
+    }
+    .center {
+      width: 70%;
+      font-size: 16px;
+      display: inline-block;
+      vertical-align: top;
+      input {
+        width: 100%;
+        height: 30px;
+        line-height: 30px;
+        background: #fff;
+        border: none;
+        padding: 0;
+        margin: 5px 0 0 0;
+        outline: none;
+        text-indent: 10px;
+        &.border {
+          box-sizing: border-box;
+          border: 1px solid #dedede;
+        }
+      }
+    }
+    .right {
+      width: 15%;
+      font-size: 16px;
+      line-height: 40px;
+      display: inline-block;
+      text-align: center;
+      padding: 0;
+      margin: -1px 0 0 0;
+      vertical-align: top;
+      &.search {
+        width: 15%;
+        height: 40px;
+        background: url('../../images/svg/search.svg') no-repeat center center;
+        background-size: 20px;
+      }
+    } 
+  }
+  .main {
+    width: 100%;
+    margin: 40px 0 0 0;
+    .title {
+      line-height: 40px;
+      font-size: 16px;
+      color: #333;
+      text-indent: 20px;
+    }
+  }   
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,6 +2,7 @@
   <div class="app">
     <div :style="indexStyle" ref="index">
       <mi-load :load="load"></mi-load>
+      <mi-header @searchEvent="searchHandle" :opac="headerOpacity"></mi-header>
       <div v-show='a'>
         <mi-footer></mi-footer>
       </div>
@@ -12,11 +13,13 @@
   import util from "../../lib/util.js"
   import load from "../components/loading.vue"
   import footer from "../components/index/footer.vue"
+  import header from "../components/index/header.vue"
 
   export default {
     components: {
       'mi-load': load,
-      'mi-footer': footer
+      'mi-footer': footer,
+      'mi-header': header
     },
     created () {
       let mi = this;
@@ -24,14 +27,32 @@
         mi.a = true;
       },2000)
     },
+    mounted () {
+      let mi = this;
+      let height = util.screenSize().width;
+      this.$refs.index.onscroll = function () {
+        mi.headerOpacity = this.scrollTop / height;
+      };
+    },
     data () {
       return {
         a: false,
         load: false,
+        searchState: false,
         indexStyle: {
           height: util.screenSize().height + 'px',
           'overfl-y': 'scroll',
           width: '100%'
+        },
+        headerOpacity: 0,
+      }
+    },
+    methods: {
+      searchHandle (Boolean) {
+        if(Boolean) {
+          this.searchState = true;
+        } else {
+          this.searchState = false;
         }
       }
     }
@@ -43,6 +64,6 @@
     background: #fff;
     overflow-y: scroll;
     width: 100%;
-    height:100%;
+    height: 100%;
   }
 </style>
